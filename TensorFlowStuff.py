3c9142555822d9d2f4053149938efc3dcb50bc29
spart <sparticlenish@gmail.com>
2018-12-15 23:28:36 -0500

    Fixed the matrix dimension problem. Need to figure out a way to take better images to test the NN

--- TensorFlowStuff.py
+++ TensorFlowStuff.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
+import cv2
+import time
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -20,11 +21,31 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=
 model.fit(x_train, y_train, epochs=3)  # train the model
 
 
-predictions = model.predict([x_test])
+TestImg = cv2.imread('Test62.jpg', 0)
+
+
+retval, procImage = cv2.threshold(TestImg, 120, 255, cv2.THRESH_BINARY_INV)
+
+
+newArray = np.zeros(shape=(10000, 28, 28))
+
+myTest = tf.convert_to_tensor(TestImg)
+
+
+
+newArray[0] = procImage
+predictions = model.predict([newArray])
 
 print(np.argmax(predictions[0]))
 
 plt.imshow(x_test[0])
+cv2.imshow('number', x_test[0])
+cv2.imshow('New Window', procImage)
+plt.imshow(procImage)
 plt.show()
 
-#plt.imshow(x_train[0])
+
+# for i in range(0, len(x_test)):
+#     cv2.imshow('number list', x_test[i])
+
+
